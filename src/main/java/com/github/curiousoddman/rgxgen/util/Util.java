836d7357a7d17ad9f2dc0f860dad82d1849cd1e5
curious_odd_man <vlad.varslavans@gmail.com>
2019-12-26 17:57:51 +0200

    #14. ^ and $ support
    Allow ^ at start and $ at end of pattern string.

--- src/main/java/com/github/curiousoddman/rgxgen/util/Util.java
+++ src/main/java/com/github/curiousoddman/rgxgen/util/Util.java
@@ -26,42 +26,6 @@ public final class Util {
         return EMPTY.split(str);
     }
 
-    public static String substringUntil(String expr, int startIndex, char c) {
-        int endIndex = startIndex;
-        while (true) {
-            // Find ending character
-            endIndex = expr.indexOf(c, endIndex);
-            if (endIndex == -1) {
-                throw new RuntimeException("Could not find character '" + c + "' in string: '" + expr.substring(startIndex));
-            }
-            int cnt = 1;        // One, because we subtract it later from endIndex. Just to avoid extra subtraction
-            // Count how much backslashes there are -
-            // Even number means that they all are escaped
-            // Odd number means that the {@code c} is escaped
-            while (expr.charAt(endIndex - cnt) == '\\') {
-                ++cnt;
-            }
-
-            // because count was 1, not 0 initially we do not equal comparison
-            if (cnt % 2 != 0) {
-                break;
-            }
-
-            // Otherwise we will find the same {@code c} at same position
-            ++endIndex;
-        }
-
-        return expr.substring(startIndex, endIndex);
-    }
-
-    public static String takeWhile(String expr, int startIndex, Predicate<Character> condition) {
-        int endIndex = startIndex;
-        while (endIndex < expr.length() && condition.test(expr.charAt(endIndex))) {
-            ++endIndex;
-        }
-        return expr.substring(startIndex, endIndex);
-    }
-
     public static String randomString(String value) {
         int count = Math.abs(value.hashCode() % 10);
         StringBuilder builder = new StringBuilder(count);
