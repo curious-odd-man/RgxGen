eede7125e847caf5b377d3a83e0d2b570a4fdd83
curious_odd_man <vlad.varslavans@gmail.com>
2019-12-29 14:42:12 +0200

    #13. Eliminate Technical Debt.
    Licence added to each source file.

--- src/main/java/com/github/curiousoddman/rgxgen/RgxGen.java
+++ src/main/java/com/github/curiousoddman/rgxgen/RgxGen.java
@@ -1,5 +1,21 @@
 package com.github.curiousoddman.rgxgen;
 
+/* **************************************************************************
+   Copyright 2019 Vladislavs Varslavans
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+/* **************************************************************************/
+
 import com.github.curiousoddman.rgxgen.generator.nodes.Node;
 import com.github.curiousoddman.rgxgen.generator.visitors.GenerationVisitor;
 import com.github.curiousoddman.rgxgen.generator.visitors.UniqueGenerationVisitor;
@@ -48,8 +64,9 @@ public class RgxGen {
 
     /**
      * Creates infinite stream of randomly generated values.
-     * @see RgxGen#generate()
+     *
      * @return stream of randomly generated strings
+     * @see RgxGen#generate()
      */
     public Stream<String> stream() {
         return Stream.generate(this::generate);
