27c1ede948749e9d6a1b5d86c7681819af1b605d
curious_odd_man <vlad.varslavans@gmail.com>
2020-04-29 16:02:17 +0300

    #32. Cap And Dollar Handling
    Visitor for simplification.

---
+++ src/main/java/com/github/curiousoddman/rgxgen/generator/visitors/SimplificationVisitor.java
@@ -0,0 +1,59 @@
+package com.github.curiousoddman.rgxgen.generator.visitors;
+
+import com.github.curiousoddman.rgxgen.generator.nodes.*;
+
+public class SimplificationVisitor implements NodeVisitor {
+    @Override
+    public void visit(SymbolSet node) {
+
+    }
+
+    @Override
+    public void visit(Choice node) {
+
+    }
+
+    @Override
+    public void visit(FinalSymbol node) {
+
+    }
+
+    @Override
+    public void visit(Repeat node) {
+
+    }
+
+    @Override
+    public void visit(Sequence node) {
+
+    }
+
+    @Override
+    public void visit(NotSymbol notSymbol) {
+
+    }
+
+    @Override
+    public void visit(GroupRef groupRef) {
+
+    }
+
+    @Override
+    public void visit(Group group) {
+
+    }
+
+    @Override
+    public void visit(LineStart lineStart) {
+
+    }
+
+    @Override
+    public void visit(LineEnd lineEnd) {
+
+    }
+
+    public Node getSimplifiedNode() {
+        return null;
+    }
+}
--- src/main/java/com/github/curiousoddman/rgxgen/simplifier/DefaultSimplifier.java
+++ src/main/java/com/github/curiousoddman/rgxgen/simplifier/DefaultSimplifier.java
@@ -1,10 +1,13 @@
 package com.github.curiousoddman.rgxgen.simplifier;
 
 import com.github.curiousoddman.rgxgen.generator.nodes.Node;
+import com.github.curiousoddman.rgxgen.generator.visitors.SimplificationVisitor;
 
 public class DefaultSimplifier implements Simplifier {
     @Override
     public Node simplify(Node input) {
-        return input;
+        SimplificationVisitor visitor = new SimplificationVisitor();
+        input.visit(visitor);
+        return visitor.getSimplifiedNode();
     }
 }
