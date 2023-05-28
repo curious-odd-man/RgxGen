f2f36d5e5c4d59ed5572a2d2d67198b5b50a62c6
curious_odd_man <vlad.varslavans@gmail.com>
2023-05-28 10:32:06 +0300

    Unit tests

--- src/test/java/com/github/curiousoddman/rgxgen/model/UnicodeCategoryTest.java
+++ src/test/java/com/github/curiousoddman/rgxgen/model/UnicodeCategoryTest.java
@@ -1,12 +1,14 @@
 package com.github.curiousoddman.rgxgen.model;
 
 import com.github.curiousoddman.rgxgen.RgxGen;
+import com.github.curiousoddman.rgxgen.iterators.StringIterator;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -37,20 +39,66 @@ class UnicodeCategoryTest {
         assertTrue(rangesPresent || symbolsPresent);
     }
 
-    public static Stream<String> getKeyAndPattern() {
+    public static Stream<String> getKey() {
         return Arrays.stream(UnicodeCategory.values())
                      .flatMap(uc -> uc.getKeys().stream());
 
     }
 
     @ParameterizedTest(name = "{index}: {0}")
-    @MethodSource("getKeyAndPattern")
-    void canGeneratedMatchingPatternTest(String key) {
+    @MethodSource("getKey")
+    void generateInCategoryTest(String key) {
         String pattern = "\\p{" + key + "}{5,10}";
         RgxGen rgxGen = new RgxGen(pattern);
         Random random = new Random(10);
+        Pattern compiled = Pattern.compile(pattern, Pattern.UNICODE_CHARACTER_CLASS);
         for (int i = 0; i < 10; i++) {
-            assertDoesNotThrow(() -> rgxGen.generate(random));
+            String s = assertDoesNotThrow(() -> rgxGen.generate(random));
+            System.out.println(s);
+            assertTrue(compiled.matcher(s).matches());
+        }
+    }
+
+    @ParameterizedTest(name = "{index}: {0}")
+    @MethodSource("getKey")
+    void generateInCategoryNotMatchingTest(String key) {
+        String pattern = "\\p{" + key + "}{5,10}";
+        RgxGen rgxGen = new RgxGen(pattern);
+        Random random = new Random(10);
+        Pattern compiled = Pattern.compile(pattern, Pattern.UNICODE_CHARACTER_CLASS);
+        for (int i = 0; i < 10; i++) {
+            String s = assertDoesNotThrow(() -> rgxGen.generateNotMatching(random));
+            System.out.println(s);
+            assertTrue(compiled.matcher(s).matches());
+        }
+    }
+
+    @ParameterizedTest(name = "{index}: {0}")
+    @MethodSource("getKey")
+    void generateUniqueTest(String key) {
+        String pattern = "\\p{" + key + "}{5,10}";
+        RgxGen rgxGen = new RgxGen(pattern);
+        Random random = new Random(10);
+        StringIterator stringIterator = rgxGen.iterateUnique();
+        Pattern compiled = Pattern.compile(pattern, Pattern.UNICODE_CHARACTER_CLASS);
+        for (int i = 0; i < 10 && stringIterator.hasNext(); i++) {
+            String s = stringIterator.next();
+            System.out.println(s);
+            assertTrue(compiled.matcher(s).matches());
+        }
+    }
+
+    @ParameterizedTest(name = "{index}: {0}")
+    @MethodSource("getKey")
+    void generateNotInCategoryTest(String key) {
+        String pattern = "\\P{" + key + "}{5,10}";
+        RgxGen rgxGen = new RgxGen(pattern);
+        Pattern compiled = Pattern.compile(pattern, Pattern.UNICODE_CHARACTER_CLASS);
+        Random random = new Random(10);
+        for (int i = 0; i < 10; i++) {
+            String s = assertDoesNotThrow(() -> rgxGen.generate(random));
+            System.out.println(s);
+            assertTrue(compiled.matcher(s).matches());
         }
     }
 }
