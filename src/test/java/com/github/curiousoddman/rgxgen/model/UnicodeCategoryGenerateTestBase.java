c91a7d29d7494f4fedfdc71291af4a5964f3d150
curious_odd_man <vlad.varslavans@gmail.com>
2023-05-29 10:02:19 +0300

    Refactoring

---
+++ src/test/java/com/github/curiousoddman/rgxgen/model/UnicodeCategoryGenerateTestBase.java
@@ -0,0 +1,108 @@
+package com.github.curiousoddman.rgxgen.model;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.function.Supplier;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static com.github.curiousoddman.rgxgen.model.UnicodeCategory.values;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.fail;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class UnicodeCategoryGenerateTestBase {
+
+    public static Stream<Arguments> getKeyAndCategory() {
+        return Arrays.stream(values())
+                     .flatMap(uc -> uc.getKeys().stream().map(k -> Arguments.of(k, uc)));
+    }
+
+    Set<UnicodeCategory>                 testedCategories;
+    Map<UnicodeCategory, Set<Character>> generatedCharacters;
+
+    @BeforeAll
+    void beforeAll() {
+        testedCategories = new HashSet<>();
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        generatedCharacters = new HashMap<>();
+    }
+
+    static Path makePathFromCategory(UnicodeCategory category) {
+        return Paths.get("testdata/" + category.name() + ".txt");
+    }
+
+    @SneakyThrows
+    Optional<Pattern> compile(String pattern, UnicodeCategory category) {
+        Files.deleteIfExists(makePathFromCategory(category));
+        try {
+            Optional<Pattern> compiledPattern = Optional.of(Pattern.compile(pattern));
+            testedCategories.add(category);
+            return compiledPattern;
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+
+    @SneakyThrows
+    void validatePattern(Optional<Pattern> compiledPattern, Supplier<String> generateFunction, UnicodeCategory category, boolean expectToMatch) {
+        String generatedText = assertDoesNotThrow(generateFunction::get);
+        //Files.write(makePathFromCategory(category), Collections.singletonList(generatedText), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+        Set<Character> generatedCharactersForCategory = generatedCharacters.computeIfAbsent(category, k -> new HashSet<>());
+        char[] generatedTextCharArray = generatedText.toCharArray();
+        for (char c : generatedTextCharArray) {
+            generatedCharactersForCategory.add(c);
+        }
+        compiledPattern.ifPresent(pattern -> {
+            if (pattern.matcher(generatedText).matches() == expectToMatch) {
+                return;
+            }
+
+            boolean[] matches = new boolean[generatedText.length()];
+            for (int i = 0; i < generatedTextCharArray.length; i++) {
+                matches[i] = pattern.matcher(String.valueOf(generatedTextCharArray[i])).matches();
+            }
+            System.out.println("Match debug:");
+            System.out.println('\t' + generatedText);
+            System.out.print('\t');
+            for (int i = 0; i < generatedTextCharArray.length; i++) {
+                System.out.print(matches[i] == expectToMatch ? " " : '!');
+            }
+            System.out.println();
+            fail("Failed for text '" + generatedText + '\'');
+        });
+    }
+
+    @AfterEach
+    void printGenerationStatistics() {
+        for (Map.Entry<UnicodeCategory, Set<Character>> entry : generatedCharacters.entrySet()) {
+            Set<Character> value = entry.getValue();
+            IntSummaryStatistics intSummaryStatistics = value.stream().mapToInt(i -> (int) i).summaryStatistics();
+            UnicodeCategory category = entry.getKey();
+            System.out.println("Category " + category + " with keys " + category.getKeys() + " character stats: ");
+            System.out.println("\tMin: " + intSummaryStatistics.getMin() + "; '" + (char) intSummaryStatistics.getMin() + '\'');
+            System.out.println("\tMax: " + intSummaryStatistics.getMax() + "; '" + (char) intSummaryStatistics.getMax() + '\'');
+            double totalValueCount = category.getSymbols().length + category.getSymbolRanges().stream().mapToInt(range -> range.getTo() - range.getFrom() + 1).sum();
+            System.out.println("\tCovered: " + (value.size() / totalValueCount));
+        }
+    }
+
+    @AfterAll
+    void verifyAllCategoriesTestedWithPatternCompile() {
+        List<UnicodeCategory> notTestedCategories = Arrays.stream(values()).filter(category -> !testedCategories.contains(category)).collect(Collectors.toList());
+        notTestedCategories.removeAll(UnicodeCategoryTest.UNCOMPILABLE_KEYS);
+        if (!notTestedCategories.isEmpty()) {
+            fail("Pattern.compile() failed for - " + notTestedCategories);
+        }
+    }
+}
--- src/test/java/com/github/curiousoddman/rgxgen/model/UnicodeCategoryTest.java
+++ src/test/java/com/github/curiousoddman/rgxgen/model/UnicodeCategoryTest.java
@@ -2,21 +2,13 @@ package com.github.curiousoddman.rgxgen.model;
 
 import com.github.curiousoddman.rgxgen.RgxGen;
 import com.github.curiousoddman.rgxgen.iterators.StringIterator;
-import lombok.SneakyThrows;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.*;
-import java.util.function.Supplier;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static com.github.curiousoddman.rgxgen.model.UnicodeCategory.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -48,119 +40,27 @@ class UnicodeCategoryTest {
 
     @ParameterizedTest
     @EnumSource(UnicodeCategory.class)
-    void keysArePresentTest(UnicodeCategory unicodeCategory) {
+    void keysAreDefinedForEachCategoryTest(UnicodeCategory unicodeCategory) {
         assertNotNull(unicodeCategory.getKeys());
         assertFalse(unicodeCategory.getKeys().isEmpty());
     }
 
     @ParameterizedTest
     @EnumSource(UnicodeCategory.class)
-    void descriptionPresentTest(UnicodeCategory unicodeCategory) {
+    void descriptionIsDefinedForEachCategoryTest(UnicodeCategory unicodeCategory) {
         assertNotNull(unicodeCategory.getDescription());
     }
 
     @ParameterizedTest
     @EnumSource(UnicodeCategory.class)
-    void symbolsOrSymbolRangesPresentTest(UnicodeCategory unicodeCategory) {
+    void isValidCategoryTest(UnicodeCategory unicodeCategory) {
         boolean rangesPresent = unicodeCategory.getSymbolRanges() != null && !unicodeCategory.getSymbolRanges().isEmpty();
         boolean symbolsPresent = unicodeCategory.getSymbols() != null && unicodeCategory.getSymbols().length != 0;
         assertTrue(rangesPresent || symbolsPresent);
     }
 
-    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-    public static class GenerateTestBase {
-
-        public static Stream<Arguments> getKeyAndCategory() {
-            return Arrays.stream(values())
-                         .flatMap(uc -> uc.getKeys().stream().map(k -> Arguments.of(k, uc)));
-
-        }
-
-        Set<UnicodeCategory>                 testedCategories;
-        Map<UnicodeCategory, Set<Character>> generatedCharacters;
-
-        @BeforeAll
-        void beforeAll() {
-            testedCategories = new HashSet<>();
-        }
-
-        @BeforeEach
-        void beforeEach() {
-            generatedCharacters = new HashMap<>();
-        }
-
-        Path makePathFromCategory(UnicodeCategory category) {
-            return Paths.get("testdata/" + category.name() + ".txt");
-        }
-
-        @SneakyThrows
-        Optional<Pattern> compile(String pattern, UnicodeCategory category) {
-            Files.deleteIfExists(makePathFromCategory(category));
-            try {
-                Optional<Pattern> compile = Optional.of(Pattern.compile(pattern));
-                testedCategories.add(category);
-                return compile;
-            } catch (Exception e) {
-                return Optional.empty();
-            }
-        }
-
-        @SneakyThrows
-        void validatePattern(Optional<Pattern> compiled, Supplier<String> generateFunction, UnicodeCategory category, boolean expectMatch) {
-            String s = assertDoesNotThrow(generateFunction::get);
-            //Files.write(makePathFromCategory(category), Collections.singletonList(s), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
-            Set<Character> characterSet = generatedCharacters.computeIfAbsent(category, k -> new HashSet<>());
-            for (char c : s.toCharArray()) {
-                characterSet.add(c);
-            }
-            compiled.ifPresent(p -> {
-                if (p.matcher(s).matches() == expectMatch) {
-                    return;
-                }
-
-                boolean[] matches = new boolean[s.length()];
-                char[] charArray = s.toCharArray();
-                for (int i = 0; i < charArray.length; i++) {
-                    char c = charArray[i];
-                    matches[i] = p.matcher("" + c).matches();
-                }
-                System.out.println("Match debug:");
-                System.out.println("\t" + s);
-                System.out.print("\t");
-                for (int i = 0; i < charArray.length; i++) {
-                    System.out.print(matches[i] == expectMatch ? " " : '!');
-                }
-                System.out.println();
-                fail("Failed for text '" + s + '\'');
-            });
-        }
-
-        @AfterEach
-        void afterEach() {
-            for (Map.Entry<UnicodeCategory, Set<Character>> entry : generatedCharacters.entrySet()) {
-                Set<Character> value = entry.getValue();
-                IntSummaryStatistics intSummaryStatistics = value.stream().mapToInt(i -> (int) i).summaryStatistics();
-                UnicodeCategory category = entry.getKey();
-                System.out.println("Category " + category + " with keys " + category.getKeys() + " character stats: ");
-                System.out.println("\tMin: " + intSummaryStatistics.getMin() + "; '" + (char) intSummaryStatistics.getMin() + '\'');
-                System.out.println("\tMax: " + intSummaryStatistics.getMax() + "; '" + (char) intSummaryStatistics.getMax() + '\'');
-                double totalValueCount = category.getSymbols().length + category.getSymbolRanges().stream().mapToInt(range -> range.getTo() - range.getFrom() + 1).sum();
-                System.out.println("\tCovered: " + (value.size() / totalValueCount));
-            }
-        }
-
-        @AfterAll
-        void afterAll() {
-            List<UnicodeCategory> notTestedCategories = Arrays.stream(values()).filter(category -> !testedCategories.contains(category)).collect(Collectors.toList());
-            notTestedCategories.removeAll(UNCOMPILABLE_KEYS);
-            if (!notTestedCategories.isEmpty()) {
-                fail("Pattern.compile() failed for - " + notTestedCategories);
-            }
-        }
-    }
-
     @Nested
-    public class GenerateInCategoryTest extends GenerateTestBase {
+    public class GenerateInCategoryTest extends UnicodeCategoryGenerateTestBase {
         @ParameterizedTest(name = "{index}: {0}")
         @MethodSource("getKeyAndCategory")
         void generateInCategoryTest(String key, UnicodeCategory category) {
@@ -175,7 +75,7 @@ class UnicodeCategoryTest {
     }
 
     @Nested
-    public class GenerateInCategoryNotMatchingTest extends GenerateTestBase {
+    public class GenerateInCategoryNotMatchingTest extends UnicodeCategoryGenerateTestBase {
 
         @ParameterizedTest(name = "{index}: {0}")
         @MethodSource("getKeyAndCategory")
@@ -191,13 +91,12 @@ class UnicodeCategoryTest {
     }
 
     @Nested
-    public class GenerateUniqueTest extends GenerateTestBase {
+    public class GenerateUniqueTest extends UnicodeCategoryGenerateTestBase {
         @ParameterizedTest(name = "{index}: {0}")
         @MethodSource("getKeyAndCategory")
         void generateUniqueTest(String key, UnicodeCategory category) {
             String pattern = "\\p{" + key + "}{5,20}";
             RgxGen rgxGen = new RgxGen(pattern);
-            Random random = new Random(pattern.hashCode());
             StringIterator stringIterator = rgxGen.iterateUnique();
             Optional<Pattern> compiled = compile(pattern, category);
             for (int i = 0; i < GENERATE_ITERATIONS && stringIterator.hasNext(); i++) {
@@ -207,7 +106,7 @@ class UnicodeCategoryTest {
     }
 
     @Nested
-    public class GenerateNotInCategoryTest extends GenerateTestBase {
+    public class GenerateNotInCategoryTest extends UnicodeCategoryGenerateTestBase {
         @ParameterizedTest(name = "{index}: {0}")
         @MethodSource("getKeyAndCategory")
         void generateNotInCategoryTest(String key, UnicodeCategory category) {
