19c8e308045b774eecea85d8644c8cfc3d1f9625
curious_odd_man <vlad.varslavans@gmail.com>
2023-06-03 20:20:27 +0300

    Fix generation of not matched sequences

--- src/test/java/com/github/curiousoddman/rgxgen/GenerationConsistencyTests.java
+++ src/test/java/com/github/curiousoddman/rgxgen/GenerationConsistencyTests.java
@@ -5,32 +5,34 @@ import com.github.curiousoddman.rgxgen.config.RgxGenProperties;
 import com.github.curiousoddman.rgxgen.data.TestPattern;
 import com.github.curiousoddman.rgxgen.data.TestPatternCaseInsensitive;
 import lombok.SneakyThrows;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.Random;
 import java.util.stream.Stream;
 
 import static com.github.curiousoddman.rgxgen.testutil.TestingUtilities.newRandom;
 import static java.util.Collections.singletonList;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assumptions.assumeFalse;
 
-public class GenerationConsistencyTests {
+/**
+ * The aim of these test is to show that the result of generation was changed by writing values generated with same seed into a text file.
+ * When a developer makes a change to a generation algorithm - the generated values will change and will pop up during commit.
+ * This is an indication that the generated values changed - there are 2 possible ways to act:
+ * 1. Change generation algorithm so that the values stay the same
+ * 2. When the change goes to release - make a note, that the generation algorithm changed and the expected values may change as well.
+ */
+@SuppressWarnings({"TestMethodWithoutAssertion"})
 
-    private static final boolean WRITE_MODE          = false;
-    private static final int     NUM_ITERATIONS      = 100;
-    private static final Path    caseInsensitivePath = Paths.get("testdata/caseinsensitive/");
-    private static final Path    caseSensitivePath   = Paths.get("testdata/casesensitive/");
+public class GenerationConsistencyTests {
+    private static final int  NUM_ITERATIONS      = 100;
+    private static final Path caseInsensitivePath = Paths.get("testdata/caseinsensitive/");
+    private static final Path caseSensitivePath   = Paths.get("testdata/casesensitive/");
 
     public static Stream<TestPatternCaseInsensitive> getCaseInsensitivePatterns() {
         return Arrays.stream(TestPatternCaseInsensitive.values());
@@ -46,7 +48,7 @@ public class GenerationConsistencyTests {
     void verifyThatAllCaseInsensitivePatternsStaysTheSameTest(TestPatternCaseInsensitive data) {
         String name = data.name();
         Path fileName = caseInsensitivePath.resolve("matching").resolve(name + ".txt").toAbsolutePath();
-        List<String> lines = getLines(fileName);
+        Files.deleteIfExists(fileName);
         RgxGen rgxGen = new RgxGen(data.getPattern());
         RgxGenProperties properties = new RgxGenProperties();
         RgxGenOption.CASE_INSENSITIVE.setInProperties(properties, true);
@@ -54,11 +56,7 @@ public class GenerationConsistencyTests {
         Random random = newRandom(17);
         for (int i = 0; i < NUM_ITERATIONS; i++) {
             String generated = rgxGen.generate(random);
-            if (WRITE_MODE) {
-                Files.write(fileName, singletonList(generated), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
-            } else {
-                assertEquals(lines.get(i), generated);
-            }
+            Files.write(fileName, singletonList(generated), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
         }
     }
 
@@ -68,7 +66,7 @@ public class GenerationConsistencyTests {
     void verifyThatAllCaseInsensitivePatternsStaysTheSameNotMatchingTest(TestPatternCaseInsensitive data) {
         String name = data.name();
         Path fileName = caseInsensitivePath.resolve("notmatching").resolve(name + ".txt").toAbsolutePath();
-        List<String> lines = getLines(fileName);
+        Files.deleteIfExists(fileName);
         RgxGen rgxGen = new RgxGen(data.getPattern());
         RgxGenProperties properties = new RgxGenProperties();
         RgxGenOption.CASE_INSENSITIVE.setInProperties(properties, true);
@@ -76,11 +74,7 @@ public class GenerationConsistencyTests {
         Random random = newRandom(17);
         for (int i = 0; i < NUM_ITERATIONS; i++) {
             String generated = rgxGen.generateNotMatching(random);
-            if (WRITE_MODE) {
-                Files.write(fileName, singletonList(generated), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
-            } else {
-                assertEquals(lines.get(i), generated);
-            }
+            Files.write(fileName, singletonList(generated), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
         }
     }
 
@@ -93,16 +87,12 @@ public class GenerationConsistencyTests {
         );
         String name = data.name();
         Path fileName = caseSensitivePath.resolve("matching").resolve(name + ".txt").toAbsolutePath();
-        List<String> lines = getLines(fileName);
+        Files.deleteIfExists(fileName);
         RgxGen rgxGen = new RgxGen(data.getPattern());
         Random random = newRandom(17);
         for (int i = 0; i < NUM_ITERATIONS; i++) {
             String generated = rgxGen.generate(random);
-            if (WRITE_MODE) {
-                Files.write(fileName, singletonList(generated), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
-            } else {
-                assertEquals(lines.get(i), generated);
-            }
+            Files.write(fileName, singletonList(generated), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
         }
     }
 
@@ -113,24 +103,12 @@ public class GenerationConsistencyTests {
     void verifyThatAllCaseSensitivePatternsStaysTheSameNotMatchingTest(TestPattern data) {
         String name = data.name();
         Path fileName = caseSensitivePath.resolve("notmatching").resolve(name + ".txt").toAbsolutePath();
-        List<String> lines = getLines(fileName);
+        Files.deleteIfExists(fileName);
         RgxGen rgxGen = new RgxGen(data.getPattern());
         Random random = newRandom(17);
         for (int i = 0; i < NUM_ITERATIONS; i++) {
             String generated = rgxGen.generateNotMatching(random);
-            if (WRITE_MODE) {
-                Files.write(fileName, singletonList(generated), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
-            } else {
-                assertEquals(lines.get(i), generated);
-            }
-        }
-    }
-
-    private static List<String> getLines(Path fileName) {
-        try {
-            return Files.readAllLines(fileName);
-        } catch (Exception e) {
-            return Collections.emptyList();
+            Files.write(fileName, singletonList(generated), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
         }
     }
 }
